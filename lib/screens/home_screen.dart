546b484b307f2d7d983a1cc5ec197cfa090ee4ab
xavir <xaviranik.me@gmail.com>
2020-04-15 00:52:14 +0600

    Added: Swipe to delete note

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:green_note/models/note_model.dart';
 import 'package:green_note/screens/note_modify_screen.dart';
+import 'package:green_note/widgets/note_delete_pop_up_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   final notes = [
@@ -50,32 +51,56 @@ class HomeScreen extends StatelessWidget {
           itemCount: notes.length,
           itemBuilder: (BuildContext context, index) {
             return GestureDetector(
-              onTap: () => Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => NoteModifyScreen(id: notes[index].id,))
-              ),
-              child: Container(
-                decoration: BoxDecoration(color: Colors.white),
-                padding: EdgeInsets.all(16.0),
-                margin: EdgeInsets.symmetric(horizontal: 0.0, vertical: 8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      notes[index].title,
-                      style: TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.green[600]),
+              onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                  builder: (_) => NoteModifyScreen(
+                        id: notes[index].id,
+                      ))),
+              child: Dismissible(
+                key: ValueKey(notes[index].id),
+                direction: DismissDirection.startToEnd,
+                onDismissed: (direction) {},
+                confirmDismiss: (direction) async {
+                  final result = await showDialog(
+                      context: context, builder: (_) => NoteDeletePopup());
+                  return result;
+                },
+                background: Container(
+                  color: Colors.red,
+                  padding: EdgeInsets.all(16.0),
+                  margin: EdgeInsets.symmetric(horizontal: 0.0, vertical: 8.0),
+                  child: Align(
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
                     ),
-                    SizedBox(
-                      height: 16.0,
-                    ),
-                    Text(
-                      'Last edited on: ${formattedDateTime(notes[index].editedAt)}',
-                      style: TextStyle(
-                          fontSize: 14.0, fontWeight: FontWeight.w200),
-                    ),
-                  ],
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(color: Colors.white),
+                  padding: EdgeInsets.all(16.0),
+                  margin: EdgeInsets.symmetric(horizontal: 0.0, vertical: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        notes[index].title,
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.green[600]),
+                      ),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Text(
+                        'Last edited on: ${formattedDateTime(notes[index].editedAt)}',
+                        style: TextStyle(
+                            fontSize: 14.0, fontWeight: FontWeight.w200),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
---
+++ lib/widgets/note_delete_pop_up_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class NoteDeletePopup extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+          'Warning',
+          style: TextStyle(
+            color: Colors.redAccent,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+      content: Text(
+        'Are you sure you want to delete this note?'
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Yes',),
+          onPressed: () {
+            Navigator.of(context).pop(true);
+          },
+        ),
+        FlatButton(
+          child: Text('No'),
+          onPressed: () {
+            Navigator.of(context).pop(false);
+          },
+        ),
+      ],
+    );
+  }
+}
