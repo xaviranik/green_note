e14b1cb2ddbc82885fb4248cf763997fe204ee91
xavir <xaviranik.me@gmail.com>
2020-04-17 11:51:31 +0600

    Added: get API request to fetch notes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:green_note/models/api_response.dart';
 import 'package:green_note/models/note_model.dart';
 import 'package:green_note/screens/note_modify_screen.dart';
 import 'package:green_note/services/note_service.dart';
 import 'package:green_note/widgets/note_delete_pop_up_widget.dart';
 
 class HomeScreen extends StatefulWidget {
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -15,15 +15,28 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 
   NoteService get service => GetIt.I<NoteService>();
-  List<Note> notes = [];
+  ApiResponse<List<Note>> _apiResponse;
+  bool _isLoading = false;
 
   String formattedDateTime(DateTime dateTime) {
     return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
   }
 
+  void _fetchNotes() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    _apiResponse = await service.getNotes();
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   void initState() {
-    notes = service.getNoteList();
+    _fetchNotes();
     super.initState();
   }
 
@@ -38,64 +51,78 @@ class _HomeScreenState extends State<HomeScreen> {
         onPressed: () => Navigator.pushNamed(context, '/note_modify'),
         child: Icon(Icons.add),
       ),
-      body: ListView.builder(
-          itemCount: notes.length,
-          itemBuilder: (BuildContext context, index) {
-            return GestureDetector(
-              onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                  builder: (_) => NoteModifyScreen(
-                        id: notes[index].id,
-                      ))),
-              child: Dismissible(
-                key: ValueKey(notes[index].id),
-                direction: DismissDirection.startToEnd,
-                onDismissed: (direction) {},
-                confirmDismiss: (direction) async {
-                  final result = await showDialog(
-                      context: context, builder: (_) => NoteDeletePopup());
-                  return result;
-                },
-                background: Container(
-                  color: Colors.red,
-                  padding: EdgeInsets.all(16.0),
-                  margin: EdgeInsets.symmetric(horizontal: 0.0, vertical: 8.0),
-                  child: Align(
-                    child: Icon(
-                      Icons.delete,
-                      color: Colors.white,
+      body: Builder(
+        builder: (_) {
+          if (_isLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (_apiResponse == null) {
+            return Center(child: Text("Something went wrong..."));
+          }
+
+          return ListView.builder(
+              itemCount: _apiResponse.data.length,
+              itemBuilder: (BuildContext context, index) {
+                return GestureDetector(
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) => NoteModifyScreen(
+                            id: _apiResponse.data[index].id,
+                          ))),
+                  child: Dismissible(
+                    key: ValueKey(_apiResponse.data[index].id),
+                    direction: DismissDirection.startToEnd,
+                    onDismissed: (direction) {},
+                    confirmDismiss: (direction) async {
+                      final result = await showDialog(
+                          context: context, builder: (_) => NoteDeletePopup());
+                      return result;
+                    },
+                    background: Container(
+                      color: Colors.red,
+                      padding: EdgeInsets.all(16.0),
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 0.0, vertical: 8.0),
+                      child: Align(
+                        child: Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
+                        alignment: Alignment.centerLeft,
+                      ),
                     ),
-                    alignment: Alignment.centerLeft,
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(color: Colors.white),
+                      padding: EdgeInsets.all(16.0),
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 0.0, vertical: 8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            _apiResponse.data[index].title,
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green[600]),
+                          ),
+                          SizedBox(
+                            height: 16.0,
+                          ),
+                          Text(
+                            'Last edited on: ${formattedDateTime(_apiResponse.data[index].editedAt ?? _apiResponse.data[index].createdAt)}',
+                            style: TextStyle(
+                                fontSize: 14.0, fontWeight: FontWeight.w200),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(color: Colors.white),
-                  padding: EdgeInsets.all(16.0),
-                  margin: EdgeInsets.symmetric(horizontal: 0.0, vertical: 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        notes[index].title,
-                        style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.green[600]),
-                      ),
-                      SizedBox(
-                        height: 16.0,
-                      ),
-                      Text(
-                        'Last edited on: ${formattedDateTime(notes[index].editedAt)}',
-                        style: TextStyle(
-                            fontSize: 14.0, fontWeight: FontWeight.w200),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          }),
+                );
+              });
+        },
+      ),
     );
   }
 }
--- lib/services/note_service.dart
+++ lib/services/note_service.dart
@@ -1,32 +1,44 @@
+import 'dart:convert';
+
+import 'package:green_note/models/api_response.dart';
 import 'package:green_note/models/note_model.dart';
+import 'package:http/http.dart' as http;
 
 class NoteService {
-  List<Note> getNoteList() {
-    return [
-      Note(
-        id: '1',
-        title: 'My awesome note 1',
-        createdAt: DateTime.now(),
-        editedAt: DateTime.now(),
-      ),
-      Note(
-        id: '2',
-        title: 'My awesome note 2',
-        createdAt: DateTime.now(),
-        editedAt: DateTime.now(),
-      ),
-      Note(
-        id: '3',
-        title: 'My awesome note 3',
-        createdAt: DateTime.now(),
-        editedAt: DateTime.now(),
-      ),
-      Note(
-        id: '4',
-        title: 'My awesome note 4',
-        createdAt: DateTime.now(),
-        editedAt: DateTime.now(),
-      ),
-    ];
+  static const API = 'http://api.notes.programmingaddict.com';
+  static const headers = {
+    'apiKey' : '64fcf854-ee5c-4ef3-8bbf-088e66faa6fb',
+  };
+
+  Future<ApiResponse<List<Note>>> getNotes() {
+    return http.get(API + '/notes', headers: headers)
+        .then((data) {
+          if(data.statusCode == 200) {
+            final jsonData = json.decode(data.body);
+            final notes = <Note>[];
+            for(var item in jsonData) {
+              final note = Note(
+                id: item['noteID'],
+                title: item['noteTitle'],
+                createdAt: DateTime.parse(item['createDateTime']),
+                editedAt: item['latestEditDateTime'] != null ? DateTime.parse(item['latestEditDateTime']) : null,
+              );
+              notes.add(note);
+            }
+
+            return ApiResponse<List<Note>>(
+              data: notes
+            );
+          }
+          return ApiResponse<List<Note>>(
+              error: true,
+              errorMessage: 'An error occured'
+          );
+    }).catchError((_) {
+      return ApiResponse<List<Note>>(
+          error: true,
+          errorMessage: 'An error occured'
+      );
+    });
   }
 }
